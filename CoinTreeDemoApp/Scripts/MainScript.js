8586c9ba97ccd62665109d71c799e3de640f7671
Nicholas <nathius17@gmail.com>
2018-01-17 17:52:59 +1100

    Encapsulate loading of stored data and clean up main script

--- CoinTreeDemoApp/Scripts/MainScript.js
+++ CoinTreeDemoApp/Scripts/MainScript.js
@@ -1,23 +1,29 @@
 ﻿$(document).ready(function () {
+    //load the previous app run prices and display as current
+    loadPricesFromCookieStorage();
+
+    //Retrieve new current prices from the api and display 
+    //automatically (rolling the cookie loaded prices into the previous slot)
+    getPrice();
+
+    //wait and refresh the prices every 30 seconds
+    setInterval(function () {
+        getPrice();
+    }, 30000);
+});
+
+function loadPricesFromCookieStorage()
+{
     //load in the previous ask and bid price from cookies if available
     var oldAsk = getCookie('askPrice');
     var oldBid = getCookie('bidPrice');
 
-    if (oldAsk && oldAsk != null && oldAsk != '' && oldAsk != 0)
-    {
+    //updates the display if the loaded price seems valid
+    if (oldAsk && oldAsk != null && oldAsk != '' && oldAsk != 0) {
         updateAskPrice(oldAsk);
     }
 
     if (oldBid && oldBid != null && oldBid != '' && oldBid != 0) {
         updateBidPrice(oldBid);
     }
-
-    //load the page and display current price
-    getPrice();
-
-    //refresh the page every 30 seconds
-    setInterval(function () {
-        console.log('ping');
-        getPrice();
-    }, 30000);
-});
+}
